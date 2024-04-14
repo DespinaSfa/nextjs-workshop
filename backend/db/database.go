17faf41e68316e5140846c4cab06f2d2a5cabb4d
Luca <luca.v.kannen@gmail.com>
2024-04-14 15:13:30 +0200

    Added function to connect to db and test connection

--- backend/db/database.go
+++ backend/db/database.go
@@ -1,8 +1,33 @@
 package db
 
-import "fmt"
+import (
+	"backend/config"
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+)
 
-func SetupDatabase() {
-	fmt.Println("Setting up database...")
-	// setup database...
+func SetupDatabase(config *config.Config) *sql.DB {
+	connectionString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		config.DBHost, config.DBPort, config.DBUser, config.DBPassword, config.DBName)
+
+	// Connect to database
+	db, err := sql.Open("postgres", connectionString)
+	if err != nil {
+		panic("error connecting to database: " + err.Error())
+	}
+
+	// Check if connection is successful
+	err = db.Ping()
+	if err != nil {
+		err := db.Close()
+		if err != nil {
+			fmt.Println("error pinging database: " + err.Error())
+		} // Close the database connection if ping fails
+		panic("error pinging database: " + err.Error())
+	}
+
+	fmt.Printf("Connected to database %s running on %s:%s\n", config.DBName, config.DBHost, config.DBPort)
+
+	return db
 }
