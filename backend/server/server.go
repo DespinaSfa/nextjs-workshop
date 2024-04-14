7654ac7cfd847443dbaa428a23419db47fb8265a
Luca <luca.v.kannen@gmail.com>
2024-04-14 15:14:13 +0200

    Minor code changes

--- backend/server/server.go
+++ backend/server/server.go
@@ -1,29 +1,26 @@
 package server
 
 import (
+	"backend/config"
 	"backend/db"
 	"fmt"
 	"github.com/go-chi/chi/v5"
 	"net/http"
-	"os"
 )
 
 func InitServer() {
-	currentDir, err := os.Getwd()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Current directory:", currentDir)
+	dbConfig := config.LoadConfig()
 
-	db.SetupDatabase()
+	db.SetupDatabase(dbConfig)
 	r := chi.NewRouter()
 
 	setupMiddleware(r)
 	setupRoutes(r)
 
 	const port int = 3001
+
 	fmt.Printf("Server running on http://localhost:%d\n", port)
-	err = http.ListenAndServe(":3001", r)
+	err := http.ListenAndServe(":3001", r)
 	if err != nil {
 		panic(err)
 	}
