89a9798c8bed41ca117985a6b6c8309e12fc15ad
Bestvater Tom <100359579+TBestvater@users.noreply.github.com>
2024-04-22 14:39:50 +0200

    import gorm and implement gorm db setup

--- backend/db/database.go
+++ backend/db/database.go
@@ -2,32 +2,38 @@ package db
 
 import (
 	"backend/config"
-	"database/sql"
 	"fmt"
 	_ "github.com/lib/pq"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-func SetupDatabase(config *config.Config) *sql.DB {
+func SetupDatabase(config *config.Config) (*gorm.DB, error) {
 	connectionString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		config.DBHost, config.DBPort, config.DBUser, config.DBPassword, config.DBName)
 
 	// Connect to database
-	db, err := sql.Open("postgres", connectionString)
+	db, err := gorm.Open(postgres.Open(connectionString), &gorm.Config{})
 	if err != nil {
-		panic("error connecting to database: " + err.Error())
+		return nil, fmt.Errorf("error connecting to database: %w", err)
 	}
 
-	// Check if connection is successful
-	err = db.Ping()
+	// Test the connection
+	sqlDB, err := db.DB()
 	if err != nil {
-		err := db.Close()
-		if err != nil {
-			fmt.Println("error pinging database: " + err.Error())
-		} // Close the database connection if ping fails
-		panic("error pinging database: " + err.Error())
+		return nil, fmt.Errorf("error getting underlying database connection: %w", err)
+	}
+
+	err = sqlDB.Ping()
+	if err != nil {
+		// Close the database connection if ping fails
+		if closeErr := sqlDB.Close(); closeErr != nil {
+			fmt.Println("error closing database connection:", closeErr)
+		}
+		return nil, fmt.Errorf("error pinging database: %w", err)
 	}
 
 	fmt.Printf("Connected to database %s running on %s:%s\n", config.DBName, config.DBHost, config.DBPort)
 
-	return db
+	return db, nil
 }
